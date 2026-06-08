c4579bb718a2f4a30346554ead96bc5ecb877d97
Juanda <juandac98@outlook.com>
2026-06-08 08:59:17 -0500

    Acabado

--- src/App.java
+++ src/App.java
@@ -1,5 +1,86 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        runLinkedList();
+        runQueue();
+        runStack();
+
+        Ejercicio1 ejercicio1 = new Ejercicio1();
+        String texto = "COMPUTACION";
+        String invertido = ejercicio1.inverString(texto);
+        System.out.println(invertido);
     }
+
+    private static void runStack() {
+        Stack<String> pila = new Stack<>();
+
+        pila.push("A");
+        pila.push("B");
+        pila.push("C");
+
+        String elemento = pila.pop();
+        System.out.println(pila.size());
+        System.out.println(elemento);
+
+        Deque<String> pila2 = new ArrayDeque<>(); //recomendada   
+        Deque<String> pila3 = new LinkedList<>();
+        pila2.push("a");
+        pila3.push("A");
+        pila.pop();
+        pila.pop();
+
+    }
+
+    private static void runQueue() {
+        Queue<String> cola = new ArrayDeque<>();
+        System.out.println("Lista Queue");
+
+        cola.offer("Jose");
+        cola.offer("Andres");
+        cola.offer("Ana");
+        System.out.println(cola.isEmpty());
+        System.out.println(cola.size());
+        
+        System.out.println(cola.peek());
+        System.out.println(cola.size());
+        
+        System.out.println(cola.poll());
+        System.out.println(cola.size());
+
+        while (!cola.isEmpty()) {
+            String cliente = cola.poll();
+            System.out.println("Atendi a " + cliente);
+        }
+
+        // ofer -> agrega al final
+        // peek -> devuelve el primero sin eliminar
+        // poll -> devuelve el primero y lo elimina 
+    }
+
+    private static void runLinkedList() {
+        System.out.println("Lista Enlazada / Linked List");
+        LinkedList<String> nombres = new LinkedList<>();  
+        System.out.println("Esta vacia? " + nombres.isEmpty());
+        System.out.println("Tamanio: " + nombres.size());
+        
+        nombres.add("Juan");
+        nombres.add("Pedro");
+        nombres.add("Maria");
+        nombres.add("Diego");
+
+        System.out.println("Tamanio: " + nombres.size());
+        System.out.println(nombres.getFirst());
+        System.out.println(nombres.get(2));
+        System.out.println(nombres.getLast());
+        nombres.peek();
+        nombres.pop();
+
+    }
+
+
 }
